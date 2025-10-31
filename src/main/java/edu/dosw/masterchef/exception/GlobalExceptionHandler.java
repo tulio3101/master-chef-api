ff8ddad34f25072f58d2940a650d6d1243ba1d2b
Tulio <tuliojr3101@gmail.com>
2025-10-30 20:16:45 -0500

    feat(core): pruebas

---
+++ src/main/java/edu/dosw/masterchef/exception/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package edu.dosw.masterchef.exception;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RecipeException.class)
+    public ResponseEntity<Map<String, Object>> handleRecipeException(RecipeException ex) {
+        Map<String, Object> body = new HashMap<>();
+        body.put("error", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(body);
+    }
+}
